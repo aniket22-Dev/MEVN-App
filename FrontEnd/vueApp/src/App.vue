5bbc0833d974240263eae9cfdc5a3f442ead7c11
Aniket Bhatnagar <86366772+aniket22-Dev@users.noreply.github.com>
2023-01-15 12:32:34 +0530

    integrated navigation Bar

--- FrontEnd/vueApp/src/App.vue
+++ FrontEnd/vueApp/src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <RouterLink to="/collection">Collection</RouterLink>
-  <RouterLink to="/admin/create">Admin</RouterLink>
+  <nav class="bg-gray-50 dark:bg-gray-700">
+    <div class="max-w-screen-xl px-4 py-3 mx-auto md:px-6">
+        <div class="flex items-center">
+            <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page"><RouterLink to="/collection">Collection</RouterLink></a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline"><RouterLink to="/admin/create">Admin</RouterLink></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+  
+  
   <RouterView></RouterView>
   
 </template>
