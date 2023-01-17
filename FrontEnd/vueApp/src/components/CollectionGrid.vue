3e830e88b96c4887275973fedfa2d44c111dcbab
Aniket Bhatnagar <86366772+aniket22-Dev@users.noreply.github.com>
2023-01-17 23:10:36 +0530

    added productID path in collection grid

--- FrontEnd/vueApp/src/components/CollectionGrid.vue
+++ FrontEnd/vueApp/src/components/CollectionGrid.vue
@@ -5,6 +5,7 @@
 
       <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         <div v-for="product in getProduct" :key="product.productId" class="group relative">
+          <a :href="`/product/${product?._id}`">
           <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
             <img :src="product.productImage" class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
           </div>
@@ -12,13 +13,14 @@
             <div>
               <h3 class="text-sm text-gray-700">
                   <span aria-hidden="true" class="absolute inset-0" />
-                  <a :href="product.productName">
+                  <a :href="`/product/${product._id}`">
                   {{ product.productName }}
                 </a>
               </h3>
             </div>
             <p class="text-sm font-medium text-gray-900">$ {{ product.productPrice }}</p>
           </div>
+        </a>
         </div>
       </div>
     </div>
