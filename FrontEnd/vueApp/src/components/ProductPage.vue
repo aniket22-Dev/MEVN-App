f209c2b878314632861ea83ade4eff55ec9dfa37
Aniket Bhatnagar <86366772+aniket22-Dev@users.noreply.github.com>
2023-02-19 20:54:29 +0530

    ProductPage css

--- FrontEnd/vueApp/src/components/ProductPage.vue
+++ FrontEnd/vueApp/src/components/ProductPage.vue
@@ -1,10 +1,60 @@
 <template>
-  <div>
-    <h2>Product Details</h2>
-    <p>{{ productDetails.productName }}</p>
-    <p>{{ productDetails.productPrice }}</p>
-    <p>{{ productDetails.productImage }}</p>
-    <p>{{ productDetails.productId }}</p>
+  <div class="bg-white">
+    <div class="pt-6">
+      <nav aria-label="Breadcrumb">
+        <ol role="list" class="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+          <li class="text-sm">
+            <a :href="productDetails.productImage" aria-current="page"
+              class="font-medium text-gray-500 hover:text-gray-600">{{
+                productDetails.productName }}</a>
+          </li>
+        </ol>
+      </nav>
+
+      <!-- Image gallery -->
+      <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+        <div class="aspect-w-3 aspect-h-4 hidden overflow-hidden rounded-lg lg:block">
+          <img :src="productDetails.productImage" class="h-full w-full object-cover object-center" />
+        </div>
+      </div>
+
+      <!-- Product info -->
+      <div
+        class="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
+        <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+          <h1 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{{ productDetails.productName }}</h1>
+        </div>
+
+        <!-- Options -->
+        <div class="mt-4 lg:row-span-3 lg:mt-0">
+          <h2 class="sr-only">Product information</h2>
+          <p class="text-3xl tracking-tight text-gray-900">$ {{ productDetails.productPrice }}</p>
+        </div>
+
+        <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">
+          <!-- Description and details -->
+          <div>
+            <h3 class="sr-only">Description</h3>
+
+            <div class="space-y-6">
+              <p class="text-base text-gray-900">{{ productDetails.productId }}</p>
+            </div>
+          </div>
+
+          <div class="mt-10">
+            <h3 class="text-sm font-medium text-gray-900">Highlights</h3>
+          </div>
+
+          <div class="mt-10">
+            <h2 class="text-sm font-medium text-gray-900">Details</h2>
+
+            <div class="mt-4 space-y-6">
+              <p class="text-sm text-gray-600">{{ productDetails.productId }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
