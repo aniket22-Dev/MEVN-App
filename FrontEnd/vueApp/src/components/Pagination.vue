cddf7a7c6cc24523bb43bb9e2756a1d610df7c27
Aniket Bhatnagar <86366772+aniket22-Dev@users.noreply.github.com>
2023-02-19 22:50:54 +0530

    Pagination Component completed

--- FrontEnd/vueApp/src/components/Pagination.vue
+++ FrontEnd/vueApp/src/components/Pagination.vue
@@ -1,59 +1,90 @@
 <template>
-    <div class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-        <div class="flex flex-1 justify-between sm:hidden">
-            <a href="#"
-                class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</a>
-            <a href="#"
-                class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</a>
-        </div>
-        <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-            <div>
-                <p class="text-sm text-gray-700">
-                    Showing
-                    {{ ' ' }}
-                    <span class="font-medium">1</span>
-                    {{ ' ' }}
-                    to
-                    {{ ' ' }}
-                    <span class="font-medium">10</span>
-                    {{ ' ' }}
-                    of
-                    {{ ' ' }}
-                    <span class="font-medium">97</span>
-                    {{ ' ' }}
-                    results
-                </p>
-            </div>
-            <div>
-                <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-                    <a href="#"
-                        class="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <!-- Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" -->
-                    <a href="#" aria-current="page"
-                        class="relative z-10 inline-flex items-center border border-indigo-500 bg-indigo-50 px-4 py-2 text-sm font-medium text-indigo-600 focus:z-20">1</a>
-                    <a href="#"
-                        class="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">2</a>
-                    <a href="#"
-                        class="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 md:inline-flex">3</a>
-                    <span
-                        class="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700">...</span>
-                    <a href="#"
-                        class="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 md:inline-flex">8</a>
-                    <a href="#"
-                        class="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">9</a>
-                    <a href="#"
-                        class="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">10</a>
-                    <a href="#"
-                        class="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">
-                        <span class="sr-only">Next</span>
-                    </a>
-                </nav>
-            </div>
-        </div>
+    <div>
+        <nav v-if="totalPages > 1" class="mt-4">
+            <ul class="pagination inline-flex -space-x-px">
+                <!-- First page link -->
+                <li :class="{ 'border-gray-300 dark:border-gray-700': currentPage === 1 }" class="border rounded-l-lg">
+                    <a @click.prevent="fetchProducts(1)" href="#"
+                        class="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">First</a>
+                </li>
+
+                <!-- Previous page link -->
+                <li :class="{ 'border-gray-300 dark:border-gray-700': currentPage === 1 }" class="border">
+                    <a @click.prevent="fetchProducts(Math.max(currentPage - 1, 1))" href="#"
+                        class="px-3 py-2 leading-tight text-gray-500 bg-white border hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Prev</a>
+                </li>
+
+                <!-- Display up to 5 pages, with ellipsis if needed -->
+                <template v-for="page in pages">
+                    <!-- Show ellipsis if this is not the first page and the previous page is more than 1 page away -->
+                    <li v-if="page > 1 && page - previousPage > 1" class="border">
+                        <span class="px-3 py-2 leading-tight text-gray-500 bg-white border">{{ String.fromCharCode(8230)
+                        }}</span>
+                    </li>
+                    <!-- Show the page link -->
+                    <li :class="{ 'border-gray-300 dark:border-gray-700': page === currentPage }" class="border">
+                        <a @click.prevent="fetchProducts(page)" href="#"
+                            class="px-3 py-2 leading-tight text-gray-500 bg-white border hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{{
+                                page }}</a>
+                    </li>
+                </template>
+
+                <!-- Next page link -->
+                <li :class="{ 'border-gray-300 dark:border-gray-700': currentPage === totalPages }" class="border">
+                    <a @click.prevent="fetchProducts(Math.min(currentPage + 1, totalPages))" href="#"
+                        class="px-3 py-2 leading-tight text-gray-500 bg-white border hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                </li>
+
+                <!-- Last page link -->
+                <li :class="{ 'border-gray-300 dark:border-gray-700': currentPage === totalPages }"
+                    class="border rounded-r-lg">
+                    <a @click.prevent="fetchProducts(totalPages)" href="#"
+                        class="px-3 py-2 leading-tight text-gray-500 bg-white border hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover">Last</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
-<script setup lang="ts">
+
+<script lang="ts">
+import { defineComponent, ref, computed, watchEffect } from 'vue'
+import { useAppStore } from '../store/appStore'
+
+export default defineComponent({
+    name: 'Pagination',
+
+    setup() {
+        const store = useAppStore()
+
+        const currentPage = ref(1)
+        const totalPages = computed(() => store.totalPages)
+        const products = computed(() => store.products)
+
+        watchEffect(() => {
+            store.fetchProducts(currentPage.value)
+        })
+
+        function fetchProducts(page: number) {
+            currentPage.value = page
+        }
+
+        const pages = computed(() => {
+            const totalPages = store.totalPages;
+            const pageNumbers = [];
+            for (let i = 1; i <= totalPages; i++) {
+                pageNumbers.push(i);
+            }
+            return pageNumbers;
+        });
+
+        return {
+            currentPage,
+            totalPages,
+            products,
+            fetchProducts,
+            pages
+        }
+    },
+})
 </script>
--- FrontEnd/vueApp/src/store/appStore.ts
+++ FrontEnd/vueApp/src/store/appStore.ts
@@ -1,121 +1,136 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 // Import axios to make HTTP requests
-import axios from "axios"
+import axios from "axios";
 
-export const useAppStore = defineStore("product",{
-    state: () => ({ 
-      products: [],
-      productName: '',
-      productPrice: '',
-      productImage: '',
-      productId: '',
-      productObjectId: '', // Add new property for ObjectId
-      loading: false,
-      submitting: false,
-      fetchingResults: false,
-      users:[],
-      name: '',
-      email:'',
-      password:'',
-      response: false
-    }),
-    getters: {
-      getProducts(state){
-        return state.products
-      },
-      getUsers(state){
-        return state.users
+export const useAppStore = defineStore("product", {
+  state: () => ({
+    products: [],
+    productName: "",
+    productPrice: "",
+    productImage: "",
+    productId: "",
+    productObjectId: "", // Add new property for ObjectId
+    loading: false,
+    submitting: false,
+    fetchingResults: false,
+    users: [],
+    name: "",
+    email: "",
+    password: "",
+    response: false,
+    totalPages: 0,
+    currentPage: 1,
+    totalItems: 0,
+  }),
+  getters: {
+    getProducts(state) {
+      return state.products;
+    },
+    getUsers(state) {
+      return state.users;
+    },
+  },
+  actions: {
+    async fetchProducts(page = 1, limit = 3) {
+      try {
+        const response = await axios.get(
+          "https://mevn-api-lzod.onrender.com/products",
+          {
+            params: { page, limit },
+          }
+        );
+
+        this.products = response.data.results;
+        this.totalPages = response.data.totalPages;
+        this.currentPage = response.data.currentPage;
+        this.totalItems = response.data.totalItems;
+      } catch (error) {
+        console.error(error);
+        // handle error here, e.g. show error message to user
       }
     },
-    actions: {
-      async fetchProducts() {
-        try {
-          this.fetchingResults = true;
-          const data = await axios.get('https://mevn-api-lzod.onrender.com/v2/getAll')
-            this.products = data.data;
-            this.fetchingResults = false;
-          }
-          catch (error) {
-            console.log("server Issue")
-        }
-      },
-      async fetchProductById(id: any) {
-        try {
-          this.fetchingResults = true;
-          const data = await axios.get(`https://mevn-api-lzod.onrender.com/v2/${id}`);
-          this.products.push(data.data);
-          this.fetchingResults = false;
-        } catch (error) {
-          console.log("Not able to fetch might be server issue");
-        }
-      },
-      async postProducts() {
-        try {
-          this.submitting = true;
-          const response = await axios.post('https://mevn-api-lzod.onrender.com/v2',{
+    async fetchProductById(id: any) {
+      try {
+        this.fetchingResults = true;
+        const data = await axios.get(
+          `https://mevn-api-lzod.onrender.com/v2/${id}`
+        );
+        this.products.push(data.data);
+        this.fetchingResults = false;
+      } catch (error) {
+        console.log("Not able to fetch might be server issue");
+      }
+    },
+    async postProducts() {
+      try {
+        this.submitting = true;
+        const response = await axios.post(
+          "https://mevn-api-lzod.onrender.com/v2",
+          {
             productName: this.productName,
             productPrice: this.productPrice,
             productImage: this.productImage,
             productId: this.productId,
-          });
-          
-          const productData = response.data;
-          this.$state.products.push(productData);
-          this.productName = "";
-          this.productPrice = "";
-          this.productImage = "";
-          this.productId = "";
-          // Store the ObjectId in the state
-          this.productObjectId = productData._id;
-          this.submitting = false;
-        }
-        catch(error) {
-          console.log("error in posting product");
-        }
-      },
-      async createUser() {
-        try{
-          this.submitting = true;
-          await axios.post('https://mevn-api-lzod.onrender.com/register',{
-            name:this.name,
+          }
+        );
+
+        const productData = response.data;
+        this.$state.products.push(productData);
+        this.productName = "";
+        this.productPrice = "";
+        this.productImage = "";
+        this.productId = "";
+        // Store the ObjectId in the state
+        this.productObjectId = productData._id;
+        this.submitting = false;
+      } catch (error) {
+        console.log("error in posting product");
+      }
+    },
+    async createUser() {
+      try {
+        this.submitting = true;
+        await axios
+          .post("https://mevn-api-lzod.onrender.com/register", {
+            name: this.name,
             email: this.email,
-            password: this.password
-          }).then((response)=>{
+            password: this.password,
+          })
+          .then((response) => {
             const userData = response.data;
             this.$state.users.push(userData);
             this.name = "";
             this.email = "";
             this.password = "";
             this.submitting = false;
-          })
-        }
-        catch(error){
-          console.log("error while creating user");
-        }
-      },
-      async loginUser() {
-        try{
-          this.submitting = true;
-          await axios.post('https://mevn-api-lzod.onrender.com/login',{
+          });
+      } catch (error) {
+        console.log("error while creating user");
+      }
+    },
+    async loginUser() {
+      try {
+        this.submitting = true;
+        await axios
+          .post("https://mevn-api-lzod.onrender.com/login", {
             email: this.email,
-            password: this.password
-          }).then((response)=>{
+            password: this.password,
+          })
+          .then((response) => {
             const userData = response.data;
             this.$state.users.push(userData);
             this.email = "";
             this.password = "";
             this.submitting = false;
-            if(response.status == 200){
+            if (response.status == 200) {
               this.response = true;
-            } else{
+            } else {
               this.response = false;
             }
-          })
-        }
-        catch(error){
-          console.log("error while creating user");
-        }
+          });
+      } catch (error) {
+        console.log("error while creating user");
       }
-   
-    }
-  })
+    },
+  },
+});
