00c7e9d707eb9dbfdde655d66b9bcb2d257555d6
Aniket Bhatnagar <86366772+aniket22-Dev@users.noreply.github.com>
2023-03-30 22:06:47 +0530

    created home page

--- FrontEnd/vueApp/src/App.vue
+++ FrontEnd/vueApp/src/App.vue
@@ -62,6 +62,13 @@
             <a href="#"
               class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
               aria-current="page">
+              <RouterLink to="/">Home</RouterLink>
+            </a>
+          </li>
+          <li>
+            <a href="#"
+              class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+              aria-current="page">
               <RouterLink to="/collection">Collection</RouterLink>
             </a>
           </li>
--- FrontEnd/vueApp/src/main.ts
+++ FrontEnd/vueApp/src/main.ts
@@ -1,26 +1,33 @@
-import { createApp } from 'vue'
-import { createRouter,createWebHistory } from 'vue-router'
-import App from './App.vue'
-import CollectionGrid from './components/CollectionGrid.vue';
-import Create from './Admin/Create.vue';
-import LoginForm from './Login/LoginForm.vue';
-import SignUp from './Login/SignUp.vue';
-import ProductPage from './components/ProductPage.vue';
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import CollectionGrid from "./components/CollectionGrid.vue";
+import Create from "./Admin/Create.vue";
+import LoginForm from "./Login/LoginForm.vue";
+import SignUp from "./Login/SignUp.vue";
+import ProductPage from "./components/ProductPage.vue";
+import HomePage from "./components/HomePage.vue";
 
 //Import Pinia into your config file
-import { createPinia } from 'pinia'
+import { createPinia } from "pinia";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path:'/collection',name: 'collection', component: CollectionGrid },
-        { path:'/admin/create',name: 'createCollection', component: Create },
-        { path:'/login',name: 'LoginForm', component: LoginForm },
-        { path:'/signup',name: 'signUp', component: SignUp },
-        { path: `/product/${window.location.href.split('/product/')[1]}`, name: 'ProductPage', component: ProductPage}
-    ]
-})
+  history: createWebHistory(),
+  routes: [
+    { path: "/collection", name: "collection", component: CollectionGrid },
+    { path: "/admin/create", name: "createCollection", component: Create },
+    { path: "/login", name: "LoginForm", component: LoginForm },
+    { path: "/signup", name: "signUp", component: SignUp },
+    {
+      path: `/product/${window.location.href.split("/product/")[1]}`,
+      name: "ProductPage",
+      component: ProductPage,
+    },
+    { path: "", name: "home", component: HomePage },
+  ],
+});
 createApp(App)
-//Add the line below to the file to instantiate it
-.use(createPinia()).use(router)
-.mount('#app')
+  //Add the line below to the file to instantiate it
+  .use(createPinia())
+  .use(router)
+  .mount("#app");
